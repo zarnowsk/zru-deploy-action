bca86e68d3b600a450d56ea2aeacba0ff8ba3da4
zarnowsk <50339865+zarnowsk@users.noreply.github.com>
2022-03-08 18:45:10 -0500

    Trying with YAML input

--- index.js
+++ index.js
@@ -16,56 +16,59 @@ const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
 const deploy = async () => {
 	try {
+		console.log(core.getMultilineInput("parameters", { required: true }));
+		console.log(core.getInput("parameters", { required: true }));
+		console.log(JSON.parse(core.getInput("parameters", { required: true })));
 		// Create the instance in the platform first
-		const instanceDeploy = await axios.post(`${base_url}/deploy`, parameters, {
-			headers,
-		});
+		// const instanceDeploy = await axios.post(`${base_url}/deploy`, parameters, {
+		// 	headers,
+		// });
 
-		if (instanceDeploy.status !== 200 && instanceDeploy.status !== 201) {
-			console.error("Something went wrong...");
-			console.error(instanceDeploy.data);
-			return;
-		} else {
-			console.log("Started deployment...");
-			const instanceId = instanceDeploy.data.instance.id;
+		// if (instanceDeploy.status !== 200 && instanceDeploy.status !== 201) {
+		// 	console.error("Something went wrong...");
+		// 	console.error(instanceDeploy.data);
+		// 	return;
+		// } else {
+		// 	console.log("Started deployment...");
+		// 	const instanceId = instanceDeploy.data.instance.id;
 
-			let i = 0,
-				keepGoing = true;
-			while (keepGoing) {
-				try {
-					const deploymentDetails = await axios.get(
-						`${base_url}/details/?instanceId=${instanceId}`,
-						{ headers }
-					);
-					const state = deploymentDetails.data.metadata.status;
-					console.log(`Current state: ${state}`);
+		// 	let i = 0,
+		// 		keepGoing = true;
+		// 	while (keepGoing) {
+		// 		try {
+		// 			const deploymentDetails = await axios.get(
+		// 				`${base_url}/details/?instanceId=${instanceId}`,
+		// 				{ headers }
+		// 			);
+		// 			const state = deploymentDetails.data.metadata.status;
+		// 			console.log(`Current state: ${state}`);
 
-					if (i > 100) {
-						console.error("Timing out after ~500 seconds.");
-						keepGoing = false;
-						return;
-					}
+		// 			if (i > 100) {
+		// 				console.error("Timing out after ~500 seconds.");
+		// 				keepGoing = false;
+		// 				return;
+		// 			}
 
-					if (state.toUpperCase() === "DEPLOYED") {
-						console.log("Deployed successfully!");
-						console.log("Output:");
+		// 			if (state.toUpperCase() === "DEPLOYED") {
+		// 				console.log("Deployed successfully!");
+		// 				console.log("Output:");
 
-						Object.entries(deploymentDetails.data.metadata.output).forEach(
-							([key, value]) => {
-								console.log(`${key}: ${value}`);
-							}
-						);
-						keepGoing = false;
-						return;
-					}
-				} catch (e) {
-					console.error(e);
-				}
+		// 				Object.entries(deploymentDetails.data.metadata.output).forEach(
+		// 					([key, value]) => {
+		// 						console.log(`${key}: ${value}`);
+		// 					}
+		// 				);
+		// 				keepGoing = false;
+		// 				return;
+		// 			}
+		// 		} catch (e) {
+		// 			console.error(e);
+		// 		}
 
-				await sleep(10000);
-				i++;
-			}
-		}
+		// 		await sleep(10000);
+		// 		i++;
+		// 	}
+		// }
 	} catch (e) {
 		console.error(e);
 	}
